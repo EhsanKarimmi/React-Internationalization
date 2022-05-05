88b9fc65751aae3276ed9b82d1557b052fbe6c93
EhsaN Karimi <ehsan.karimmi@gmail.com>
2022-05-06 01:21:22 +0430

    Change i18n loadPath (#2)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,31 +1,34 @@
 import i18n from "i18next";
-import Backend from "i18next-xhr-backend";
+import HttpApi from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import enTranslation from "../public/locales/en/translation.json";
-import faTranslation from "../public/locales/fa/translation.json";
+// import enTranslation from "../public/locales/en/translation.json";
+// import faTranslation from "../public/locales/fa/translation.json";
 
-const resources = {
-    en: {
-        translation: enTranslation,
-    },
-    fa: {
-        translation: faTranslation,
-    },
-};
+// const resources = {
+//     en: {
+//         translation: enTranslation,
+//     },
+//     fa: {
+//         translation: faTranslation,
+//     },
+// };
 
-i18n.use(Backend)
+i18n.use(HttpApi)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: "en",
         debug: true,
-        resources,
         interpolation: {
             escapeValue: false,
         },
         detection: {
             lookupLocalStorage: "Github_Club_Lang",
+        },
+        backend: {
+            //                 ⬇️
+            loadPath: "/React-Internationalization/locales/{{lng}}/translation.json",
         },
     });
 
