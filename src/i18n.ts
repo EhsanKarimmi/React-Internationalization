c77049491a32f321795364092b51d1fe3311ec4d
EhsaN Karimi <ehsan.karimmi@gmail.com>
2022-05-06 01:00:35 +0430

    Change backend loadPath (#3)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,20 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
+import Backend from "i18next-http-backend";
+// import HttpApi from "i18next-http-backend";
 // import XHR from "i18next-xhr-backend";
 
-i18n.use(HttpApi)
+i18n.use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        debug: true,
         fallbackLng: "en",
-        backend: {
-            loadPath: "/locales/{{lng}}/translation.json",
-            allowMultiLoading: false,
-        },
         interpolation: {
             escapeValue: false,
+        },
+        backend: {
+            loadPath: "/public/locales/{{lng}}/translation.json",
         },
         detection: {
             lookupLocalStorage: "Github_Club_Lang",
