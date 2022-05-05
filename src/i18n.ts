dd6582806dad881052c66d13a244cc96a6ca90ba
EhsaN Karimi <ehsan.karimmi@gmail.com>
2022-05-06 00:13:13 +0430

    Change backend loadPath (#2)

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,14 +2,20 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
+import XHR from "i18next-xhr-backend";
 
-i18n.use(Backend)
+i18n.use(XHR)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: "en",
+        // backend: {
+        //     loadPath: "../public/locales/{{lng}}/translation.json",
+        //     allowMultiLoading: false,
+        // },
+
         backend: {
-            loadPath: "./locales/{{lng}}/translation.json",
+            loadPath: "/locales/{{lng}}/translation.json",
             allowMultiLoading: false,
         },
         interpolation: {
