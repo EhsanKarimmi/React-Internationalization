8dc849a04f6b722334adf36cc7be3c4d2db3b5f5
EhsaN Karimi <ehsan.karimmi@gmail.com>
2022-05-06 00:36:27 +0430

    Change i18n load path

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import XHR from "i18next-xhr-backend";
+import HttpApi from "i18next-http-backend";
+// import XHR from "i18next-xhr-backend";
 
-i18n.use(XHR)
+i18n.use(HttpApi)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: "en",
-        // backend: {
-        //     loadPath: "../public/locales/{{lng}}/translation.json",
-        //     allowMultiLoading: false,
-        // },
-
         backend: {
             loadPath: "/locales/{{lng}}/translation.json",
             allowMultiLoading: false,
