5b6f7766616c62f7b3a452957180ecffb2989d90
EhsaN Karimi <ehsan.karimmi@gmail.com>
2022-05-06 01:36:25 +0430

    Add fallback loading component

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,17 +2,6 @@ import i18n from "i18next";
 import HttpApi from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-// import enTranslation from "../public/locales/en/translation.json";
-// import faTranslation from "../public/locales/fa/translation.json";
-
-// const resources = {
-//     en: {
-//         translation: enTranslation,
-//     },
-//     fa: {
-//         translation: faTranslation,
-//     },
-// };
 
 i18n.use(HttpApi)
     .use(LanguageDetector)
@@ -27,7 +16,6 @@ i18n.use(HttpApi)
             lookupLocalStorage: "Github_Club_Lang",
         },
         backend: {
-            //                 ⬇️
             loadPath: "/React-Internationalization/locales/{{lng}}/translation.json",
         },
     });
